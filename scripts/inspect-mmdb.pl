b604df604eb09ebcb8cf80123556ff34c273edfe
thisisaaronland <aaron@mapzen.com>
2016-01-12 19:48:50 +0000

    first pass to recurse through an mmdb file documenting types

---
+++ scripts/inspect-mmdb.pl
@@ -0,0 +1,111 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+use Getopt::Std;
+
+use MaxMind::DB::Reader;
+use Scalar::Util 'reftype';
+
+{
+    &main();
+    exit();
+}
+
+sub main {
+
+    my %opts = ();
+
+    getopts('s:', \%opts);
+
+    my $src = $opts{'s'};
+
+    if (! $src){
+	print "source parameter is missing\n";
+	return 0;
+    }
+
+    if (! -e $src){
+	print "source does not exist\n";
+	return 0;
+    }
+
+    my $reader = MaxMind::DB::Reader->new(file => $src);
+    # my $meta = $reader->metadata();
+
+    my %report = ();
+
+    my $callback = sub {
+	    my $ip_as_integer = shift;		# ignore
+	    my $mask_length   = shift;		# ignore
+	    my $data          = shift;
+
+	    &reporter("", $data, \%report);
+    };
+
+    $reader->iterate_search_tree($callback);
+
+    print Dumper(\%report);
+}
+
+sub reporter {
+    my $parent = shift;
+    my $subject = shift;
+    my $report = shift;
+    
+    my %scalars = (
+	'latitude' => 'double',
+	'longitude' => 'double',
+	'geoname_id' => 'uint64',
+	);
+
+    my $isa = ref($subject) || ref(\$subject);
+    
+    if ($isa eq "HASH"){
+	
+	foreach my $k (keys %$subject){
+	    
+	    if (! $report->{$k}){
+		$report->{$k} = "map";
+	    }
+	    
+	    &reporter($k, $subject->{$k}, $report);
+	}
+    }
+
+    elsif ($isa eq "ARRAY"){
+
+	if (! $report->{$parent}){
+
+	    my $first = $subject->[0];
+	    my $first_isa = ref($first) || ref(\$first);
+
+	    print "$parent is ARRAY first is $first_isa\n";
+	
+	    $report->{$parent} = [ 'array', $first_isa ];
+	}
+
+	foreach my $item (@$subject){
+	    &reporter("", $item, $report);
+	}
+    }
+
+    elsif ($isa eq "SCALAR"){
+	
+	if (! $report->{$parent}){
+
+	    my $type = $scalars{$parent} || "utf8_string";
+	    $report->{$parent} = $type;
+	}
+    }
+    
+    else {
+	print "SUBJECT ($parent) IS A $isa\n";
+	print Dumper($subject);
+	exit();
+    }
+
+}
+
